531efad17d4802910147df7689533cb9d8e72856
OysteinKoder <oystein.kodehode@gmail.com>
2023-09-27 12:53:30 +0200

    Start of javascript lesson, will be continued

---
+++ part_one_arrays_objects.js
@@ -0,0 +1,16 @@
+// Arrays
+const myArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+console.log(myArray);
+console.table(myArray);
+
+//
+//
+
+// Objects
+const myObject = {
+  nameKey: "John",
+  surnameKey: "Doe",
+  ageKey: 30,
+};
+
+console.log(myObject);
