9db07b43fb377faa55cdb5b34e4d925b9f5535b9
Julie <julie.kodehode@gmail.com>
2023-10-04 11:31:02 +0200

    Update from wednesday lesson

--- oppgave/scripts.js
+++ oppgave/scripts.js
@@ -1,30 +1,31 @@
 console.log("Hello World!");
 
 // Hente HTML element
-const plusButton = document.querySelector("#plusButton");
+const displayNumber = document.querySelector("#displayNumberID");
+console.log(displayNumber);
+const plusButton = document.querySelector("#plusButtonID");
 console.log(plusButton);
-const minusButton = document.querySelector("#minusButton");
+const minusButton = document.querySelector("#minusButtonID");
 console.log(minusButton);
 
-// Vi må sette opp variabler som rekner
+// Vi må sette opp variabler som regner
 let count = 0;
 
 // Lage funksjoner til knappene
 function increment() {
 	count = count + 1;
 	console.log(count);
+	displayNumber.textContent = count;
 }
-increment();
-increment();
-increment();
-increment();
-increment();
 
+// En funksjon som tar i mot variablelen count og gjør et regnestykke som blir lagt til i nettsiden med textContent
 function decrement() {
 	count = count - 1;
 	console.log(count);
+	displayNumber.textContent = count;
 }
-decrement();
-decrement();
-increment();
-increment();
+
+// Denne event lytteren tar en allerede laget funksjon som sitt andre parament
+plusButton.addEventListener("click", increment);
+
+minusButton.addEventListener("click", decrement);
