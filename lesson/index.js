aac9889a42db3ef389c11fda1ede53143a7650c9
OysteinKoder <oystein.kodehode@gmail.com>
2023-10-10 09:45:06 +0200

    changes from lesson

--- lesson/index.js
+++ lesson/index.js
@@ -1,43 +1,89 @@
 // Arrays
-const myArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
-console.log(myArray);
-console.table(myArray);
+const exampleArray = ["bil", "hund", "katt", "hest"];
 
-// Array Methods
+// Array Methods basic
+exampleArray.unshift("klovn");
+
+exampleArray.shift();
+
+exampleArray.push("raggesokker");
+
+exampleArray.pop();
+
+console.log(exampleArray);
 
 // Objects
-const myObject = {
-  nameKey: "John",
-  surnameKey: "Doe",
-  ageKey: 30,
-};
-console.log(myObject);
 
-// Object Methods
+const exampleObject = {
+  name: "john",
+  surname: "doug",
+  occupation: "baker",
+  age: 59,
+};
+
+console.log(exampleObject.surname);
+
+// Array/Object
+
+const arrayObjectMix = [
+  {
+    dogRace: "korgi",
+    dogAge: 7,
+    furColor: "red",
+  },
+  {
+    catRace: "Blue Russian",
+    catLives: 8,
+    furColor: "Kinda blue",
+    catsMyAuntOwns: ["Luna", "Muffin", "Shadow", "Ginger", "Midnight", "Cleo"],
+  },
+];
+
+console.log(arrayObjectMix[0].dogAge);
 
 // Loops.
 
+const arrayLoop = [
+  "hey world",
+  "hey world",
+  "hey world",
+  "hey world",
+  "hey world",
+  "hey world",
+  "hey world",
+  "hey world",
+];
+
+// ok. Så når vi vil loope igjennom et helt array,
+// så kan vi bruke array.length
+
+for (i = 0; i < arrayLoop.length; i++) {
+  console.log(arrayLoop[i]);
+}
+
+// alle gå inn og stem på poll
+
 // javascript to manipulate the dom
 
-const insertElementHere = document.querySelector("#insert-element-here");
+// const insertElementHere = document.querySelector("#insert-element-here");
 
-const newElement = document.createElement("p");
+// const newElement = document.createElement("p");
 
-newElement.id = "new-element";
+// newElement.id = "new-element";
 
-insertElementHere.appendChild(newElement);
+// insertElementHere.appendChild(newElement);
 
-newElement.remove();
+// newElement.remove();
 
 // With function
 
-newElement.innerHTML = "This is a new element!";
+// newElement.innerHTML = "This is a new element!";
 
-function createNewElement() {
-  const newElement = document.createElement("p");
-  newElement.style.border = "blue 1px solid";
-  insertElementHere.appendChild(newElement);
-  newElement.innerHTML = "This is a new element!";
+// function createNewElement() {
+//   const newElement = document.createElement("p");
+//   newElement.style.border = "blue 1px solid";
+//   insertElementHere.appendChild(newElement);
+//   newElement.innerHTML = "This is a new element!";
 
-  console.log("This is a new element!");
-}
+//   console.log("This is a new element!");
+// }
