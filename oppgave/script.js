e08267508d46d7a19f11367cc8a4582a895e5ae0
Juliekodehode <julie.kodehode@gmail.com>
2023-10-30 13:40:39 +0100

    Lagt til kommentarer

--- oppgave/script.js
+++ oppgave/script.js
@@ -1,5 +1,9 @@
+// Import the "themeToggle" function from a module located in "themeToggle.js"
 import { themeToggle } from "./themeToggle.js";
 
+// Find and store a reference to the <button> element in the document
+// In this instance I have directly targeted the element, normally we would target an ID or class name
 const button = document.querySelector("button");
 
+// Add an event listener to the button, listening for a "click" event, and preforming the function when activated
 button.addEventListener("click", themeToggle);
--- oppgave/themeToggle.js
+++ oppgave/themeToggle.js
@@ -1,5 +1,12 @@
+// Define a function named "themeToggle"
 export function themeToggle() {
+	// Find and store a reference to the <body> element in the document
+	// In this instance I have directly targeted the element, normally we would target an ID or class name
 	const body = document.querySelector("body");
+
+	// Toggle the class "darkMode" on the <body> element
 	body.classList.toggle("darkMode");
+
+	// Logging a message to the console indicating that the button was clicked
 	console.log("Button clicked?");
 }
